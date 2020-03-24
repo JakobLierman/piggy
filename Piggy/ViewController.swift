04f4af2cbd108d5d6305283a0d1ede0cf42c1644
Jakob Lierman <jakob.lierman@gmail.com>
2020-03-24 19:27:48 +0100

    Initial Commit

---
+++ Piggy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Piggy
+//
+//  Created by Jakob Lierman on 24/03/2020.
+//  Copyright © 2020 Jakob Lierman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
