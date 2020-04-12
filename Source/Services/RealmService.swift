95b6c99da3e84d90e00d788208521fb7ceebccd3
Jakob Lierman <jakob.lierman@gmail.com>
2020-04-12 16:33:03 +0200

    Realm Service

---
+++ Source/Services/RealmService.swift
@@ -0,0 +1,65 @@
+//
+//  RealmService.swift
+//  Piggy
+//
+//  Created by Jakob Lierman on 05/04/2020.
+//  Copyright © 2020 Jakob Lierman. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmService {
+    
+    private init() {}
+    static let shared = RealmService()
+    
+    var realm = try! Realm()
+    
+    func create<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.add(object)
+            }
+        } catch {
+            post(error)
+        }
+    }
+    
+    func update<T: Object>(_ object: T, with dictionary: [String: Any?]) {
+        do {
+            try realm.write {
+                for (key, value) in dictionary {
+                    object.setValue(value, forKey: key)
+                }
+            }
+        } catch {
+            post(error)
+        }
+    }
+    
+    func delete<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch {
+            post(error)
+        }
+    }
+    
+    func post(_ error: Error) {
+        NotificationCenter.default.post(name: NSNotification.Name("RealmError"), object: error)
+    }
+    
+    func observeRealmErrors(in viewController: UIViewController, completion: @escaping (Error?) -> Void) {
+        NotificationCenter.default.addObserver(forName: NSNotification.Name("RealmError"), object: nil, queue: nil) { (notification) in
+            completion(notification.object as? Error)
+        }
+    }
+    
+    func stopObservingErrors(in viewController: UIViewController) {
+        NotificationCenter.default.removeObserver(viewController, name: NSNotification.Name("RealmError"), object: nil)
+    }
+    
+}
