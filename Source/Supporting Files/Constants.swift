c3ee571fee994741e1b66000ad5cd9a8967cddcc
Jakob Lierman <jakob.lierman@gmail.com>
2020-04-17 23:19:16 +0200

    Cleanup

---
+++ Source/Supporting Files/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Piggy
+//
+//  Created by Jakob Lierman on 05/04/2020.
+//  Copyright © 2020 Jakob Lierman. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+
+}
