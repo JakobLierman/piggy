0cc952798e95ad82ef362c4bc2aff11ce2df59af
Jakob Lierman <jakob.lierman@gmail.com>
2020-04-20 23:37:05 +0200

    Cleanup

--- Pods/Target Support Files/Pods-Piggy/Pods-Piggy-frameworks.sh
+++ Pods/Target Support Files/Pods-Piggy/Pods-Piggy-frameworks.sh
@@ -199,11 +199,13 @@ fi
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SPLarkController/SPLarkController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TORoundedButton/TORoundedButton.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SPLarkController/SPLarkController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TORoundedButton/TORoundedButton.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
